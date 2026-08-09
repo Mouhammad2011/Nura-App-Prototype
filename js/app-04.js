57ade33cdacc0baba223074c6a905d23c790038b
Germark <mouhammad.hameed24311@gmail.com>
2026-08-09 18:29:29 +0200

    Delete js directory

--- js/app-04.js
+++
@@ -1,5 +0,0 @@
-
-/* Close Add Folder modal on backdrop click */
-document.getElementById('nura-add-folder-modal').addEventListener('click', function(e){
-  if(e.target === this) closeAddFolderModal();
-});
